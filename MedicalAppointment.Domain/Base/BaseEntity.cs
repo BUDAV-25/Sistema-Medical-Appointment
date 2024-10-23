5fcb3caeb84a26318d108ccc6b3d586eec4aa0ec
acevedorandy <acevedorandy70@gmail.com>
2024-10-23 04:36:56 -0400

    Probando conexión

--- MedicalAppointment.Domain/Base/BaseEntity.cs
+++ MedicalAppointment.Domain/Base/BaseEntity.cs
@@ -2,6 +2,8 @@
 {
     public abstract class BaseEntity
     {
+        //Probando conexión
+        // Benny 
         private DateTime CreatedAt { get; set; }
         private DateTime UpdatedAt { get; set; }
         private bool IsActive { get; set; }
