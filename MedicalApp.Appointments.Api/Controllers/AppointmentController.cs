409b2bc671ecc0c0e776a2c5080a3df052007908
acevedorandy <acevedorandy70@gmail.com>
2024-11-01 04:03:58 -0400

    Trabajando con las API´S

---
+++ MedicalApp.Appointments.Api/Controllers/AppointmentController.cs
@@ -0,0 +1,59 @@
+﻿using MedicalAppointment.Persistance.Interfaces.appointments;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MedicalApp.Appointments.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IAppointmentsRepository _appointmentsRepository;
+        public AppointmentController(IAppointmentsRepository appointmentsRepository)
+        {
+            _appointmentsRepository = appointmentsRepository;
+
+
+
+        }
+        [HttpGet("GettAppointment")]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _appointmentsRepository.GetAll();
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+
+
+        }
+
+        // GET api/<AppointmentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AppointmentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AppointmentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AppointmentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
