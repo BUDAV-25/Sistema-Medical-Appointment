8e872249cdc9a408e26903f77567b1831d33a453
jepherson1 <logangameryt11@gmail.com>
2024-11-11 03:13:32 -0400

    implementacionde la capa de servivicio

--- MedicalApp.Insurance.Api/Program.cs
+++ MedicalApp.Insurance.Api/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MedicalAppointmentContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("insuranceDB")));
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
